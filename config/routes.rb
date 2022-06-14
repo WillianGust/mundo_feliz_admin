296d860131ed2977718c94edd285476aeae8a1d5
WillianGust <williandes.silva@yahoo.ca>
2022-06-13 23:57:29 -0400

    Modelos iniciais

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :produtos
+  root to: 'home#index'
+  get '/home', to: 'home#index'
+  get 'home/index'
+  resources :tipo_produtos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
