f35b4406688d77e1ac813cc90df8947e1025259b
WillianGust <williandes.silva@yahoo.ca>
2022-06-14 23:53:44 -0400

    Criando migration, alterando views index, form, show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_183703) do
+ActiveRecord::Schema.define(version: 2022_06_15_032303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_06_14_183703) do
     t.bigint "tipo_produto_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "valor"
     t.index ["tipo_produto_id"], name: "index_produtos_on_tipo_produto_id"
   end
 
