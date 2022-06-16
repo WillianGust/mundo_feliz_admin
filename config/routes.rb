ff0a2e1ce07c1dc261d2bd3589ad05e47cea4663
WillianGust <williandes.silva@yahoo.ca>
2022-06-16 15:36:59 -0400

    Pagina login, controller e view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :administradors
   resources :pedidos do 
     resources :pedido_produtos
   end
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/home', to: 'home#index'
   get 'home/index'
+  get '/login', to: 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
