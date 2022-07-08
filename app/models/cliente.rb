ad3f32fbee1c125a79659e221fc61589a7c781a7
WillianGust <williandes.silva@yahoo.ca>
2022-07-08 14:55:06 -0400

    Correcao javascript e criaca do meio de pagamento Iugu

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,15 @@
 class Cliente < ApplicationRecord
   validates :nome, :cpf, :telefone, :senha, :email, presence: true
+
+  def nome_tratado
+    nome = self.nome.split(" ").first
+  end
+
+  def sobrenome
+    nome = self.nome.split(" ").first
+    sobrenome = self.nome.split(" ").last
+    return "" if nome == sobrenome
+    sobrenome
+  end 
+
 end
